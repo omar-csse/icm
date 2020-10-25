c13cb6f720ecf5fe15185e4c6fe54bf530c84f70
omar <omar.csse@gmail.com>
2020-10-25 22:33:57 +1000

    finish logic

--- poker/poker.js
+++ poker/poker.js
@@ -4,33 +4,21 @@ const fs = require('fs');
 
 let PokerGame = class Poker {
 
-    player1_ranks = {}
-    player2_ranks = {}
-    player1_cards = []
-    player2_cards = []
+    player1_winning = 0
+    player2_winning = 0
+    player1_hands = []
+    player2_hands = []
+    player1_hands_sorted = []
+    player2_hands_sorted = []
 
     cards = new Map([
-        ['2',0], ['3',1], ['4',2], ['5',3],
-        ['6',4], ['7',5], ['8',6], ['9',7],
-        ['10',8], ['J',9], ['Q',10], ['K',11],
-        ['A',12]
+        ['2', 0], ['3', 1], ['4', 2], ['5', 3], 
+        ['6', 4], ['7', 5], ['8', 6], ['9', 7], 
+        ['T', 8], ['J', 9], ['Q', 10], ['K', 11],
+        ['A', 12]
     ])
 
-    HANDVALUES = {
-        ROYALFULUSH: 10,
-        STRAIGHTFLUSH: 9,
-        QUADS: 8,
-        FULLHOUSE: 7,
-        FLUSH: 6,
-        STRAIGHT: 5,
-        TRIPS: 4,
-        TWOPAIR: 3,
-        PAIR: 2,
-        HIGHCARD: 1
-    };
-
-
-    readCards = async () => {
+    readHands = async () => {
         
         return new Promise((resolve, reject) => {
             const readPokerLine = readline.createInterface({
@@ -40,9 +28,19 @@ let PokerGame = class Poker {
         
             readPokerLine.on('line', (line) => {
     
-                let cards = line.split(" ", 10)
-                this.player1_cards.push(cards.slice(0, 5))
-                this.player2_cards.push(cards.slice(5, 10))
+                let hands = line.split(" ", 10)
+                let hand1 = hands.slice(0, 5)
+                let hand2 = hands.slice(5, 10)
+                this.player1_hands.push(hand1)
+                this.player2_hands.push(hand2)
+
+                let hand_values = hands.map(card => this.cards.get(card[0]))
+
+                let player1_hand = hand_values.slice(0, 5).sort((a, b) => a = b)
+                let player2_hand = hand_values.slice(5, 10).sort((a, b) => a = b)
+                
+                this.player1_hands_sorted.push(player1_hand)
+                this.player2_hands_sorted.push(player2_hand)
     
             }).on('close', () => {
                 console.log(`Setup cards`)
@@ -54,37 +52,122 @@ let PokerGame = class Poker {
         
     }
 
-    player1_hand = (game) => {
-        return this.player1_cards[game]
+    frequencies = (hand) => {
+        
+        let freq = new Array(14)
+        freq.fill(0, 0, 14)
+
+        for (let i = 0; i < hand.length; i++) {
+            freq[hand[i]] += 1;
+        }
+    
+        return freq
     }
 
-    player2_hand = (game) => {
-        return this.player2_cards[game]
+    consecutive = (hand) => {
+
+        let count = 1
+        let max = 0;
+    
+        for (let i = 0; i <= hand.length; i++) {
+            if (hand[i] === hand[i-1] + 1) {
+                count++;
+                max < count ? max = count: count = 1;
+            }
+        }
+        
+        return max
+    }
+
+    arrEq = (arr1, arr2) => {
+        let arrLength;
+        if ((arrLength = arr1.length) != arr2.length) return false;
+        for (let i = 0; i < arrLength; i++) if (arr1[i] !== arr2[i]) return false;
+        return true;
+    }
+
+    check_straight = async (hand) => {
+        let res = await this.consecutive(hand)
+        if (res === 5) return true
+        return false
+    }
+
+    check_flush = async (hand_original) => {
+    
+        let suits = hand_original.map(card => card.slice(-1))
+        let res = await suits.every(suit => suit === suits[0])
+        return res
+    }
+
+    check_royal_flush = async (hand) => {
+        let royal = [8, 9, 10, 11, 12]
+        if (this.arrEq(royal, hand)) return true
+        return false
+    }
+
+    hand_rank = async (hand_original, hand) => {
+
+        let freq = this.frequencies(hand)
+    
+        let rank = 0
+        let pairs = 0
+        let hasTrips = false
+        let hasQuads = false
+        let hasFlush = false
+        let hasStraight = false
+        let isRoyal = false
+    
+        for (let i = 0; i < freq.length; i++) {
+            if (freq[i] === 2) pairs++
+            else if (freq[i] === 3) hasTrips = true
+            else if (freq[i] === 4) hasQuads = true
+        }
+    
+        if (pairs < 1 && !hasTrips && !hasQuads) {
+            hasStraight = await this.check_straight(hand)
+            hasFlush = await this.check_flush(hand_original)
+            isRoyal = await this.check_royal_flush(hand)
+        }
+    
+        if (hasFlush && isRoyal) rank = 10
+        else if (hasStraight && hasFlush) rank = 9
+        else if (hasQuads) rank = 8
+        else if (hasTrips && pairs == 1) rank = 7
+        else if (hasFlush) rank = 6
+        else if (hasStraight) rank = 5
+        else if (hasTrips) rank = 4
+        else if (pairs == 2) rank = 3
+        else if (pairs == 1) rank = 2
+        else rank = 1
+    
+        return rank
     }
     
-    same_rank = () => {
+    same_rank = (hand1, hand2) => {
+        let hand1_max = Math.max.apply(Math, hand1);
+        let hand2_max = Math.max.apply(Math, hand2);
+        return hand1_max > hand2_max ? 1 : 2
+    }
 
-        let rank = 0
+    play = async () => {
+        
+        await this.readHands()
 
-        for (let i = 0; i <= this.player1_cards.length; i++) {
-            
+        for (let i = 0; i < this.player1_hands.length; i++) {
+            let rank_p1 = await this.hand_rank(this.player1_hands[i], this.player1_hands_sorted[i])
+            let rank_p2 = await this.hand_rank(this.player2_hands[i], this.player2_hands_sorted[i])
+
+            if (rank_p1 > rank_p2) this.player1_winning++
+            else if (rank_p2 > rank_p1) this.player2_winning++
+            else {
+                let winner = this.same_rank(this.player1_hands_sorted[i], this.player2_hands_sorted[i])
+                winner === 1 ? this.player1_winning++ : this.player2_winning++
+            }
         }
-    }
 
-    is_staright = () => {
+        console.log(`player1: ${this.player1_winning} hands`)
+        console.log(`player2: ${this.player2_winning} hands`)
 
-    }
-
-    is_flush = () => {
-        
-    }
-
-    is_straight_flush = () => {
-        
-    }
-
-    is_straight_flush = () => {
-        
     }
 
 }
@@ -92,8 +175,7 @@ let PokerGame = class Poker {
 
 let main = async () => {
     let Poker = new PokerGame()
-    await Poker.readCards()
-    console.log(Poker.player1_hand(1))
+    Poker.play()
 }
 
 main()
--- poker/testing.js
+++ poker/testing.js
@@ -1,35 +1,54 @@
 cards = new Map([
-    ['2', 0],
-    ['3', 1],
-    ['4', 2],
-    ['5', 3],
-    ['6', 4],
-    ['7', 5],
-    ['8', 6],
-    ['9', 7],
-    ['10', 8],
-    ['J', 9],
-    ['Q', 10],
-    ['K', 11],
-    ['A', 12]
+    ['2', 0], ['3', 1], ['4', 2], ['5', 3], 
+    ['6', 4], ['7', 5], ['8', 6], ['9', 7], 
+    ['10', 8], ['J', 9], ['Q', 10], ['K', 11],
+    ['A', 12], ['T', 13]
 ])
 
 
-hands = ['10C', 'JC', 'QC', 'KC', 'AD']
-// hands = ['10C', 'JD', 'QD', 'KC', 'AC']
-// hands = ['4C', '5D', '6D', '7C', '8C']
-// hands = ['8C', '4D', '6D', '7C', '5C']
+hand = ['10C', 'JC', 'QC', 'KC', 'AC']
+
+// hand = ['4C', '5C', '6C', '7C', '8C']
+// hand = ['10C', 'JC', 'QC', 'KC', 'AC']
+
+// hand = ['10C', '10D', '10H', '10S', 'AC']
+
+// hand = ['8C', '8D', '8H', '7C', '7H']
+
+// hand = ['10C', 'JC', '8C', 'KC', 'AC']
+
+// hand = ['10C', 'JC', 'QH', 'KC', 'AC']
+
+// hand = ['4C', '4H', 'QH', 'KC', '4D']
+
+// hand = ['4C', '4H', 'QH', 'KC', 'QD']
+
+// hand = ['4C', '3H', 'QH', 'KC', 'QD']
+
 
 read_cards = async () => {
-    let hand_values = await hands.map(card => cards.get(card[0] == '1' ? card.substring(0,2) : card[0]))
+    let hand_values = await hand.map(card => cards.get(card[0] == '1' ? card.substring(0,2) : card[0]))
     let hand_values_sorted = await hand_values.sort((a, b) => a - b)
 
-    return { hands, hand_values, hand_values_sorted }
+    return { hand, hand_values, hand_values_sorted }
+}
+
+frequencies = async () => {
+    let { hand, hand_values, hand_values_sorted } = await read_cards()
+
+    let freq = new Array(13)
+    freq.fill(0, 0, 13)
+
+    for (let i = 0; i < hand.length; i++) {
+        freq[hand_values_sorted[i]] += 1;
+    }
+
+    return freq
 }
 
 consecutive = async () => {
 
-    let { hands, hand_values, hand_values_sorted } = await read_cards()
+    let { hand, hand_values, hand_values_sorted } = await read_cards()
     let count = 1
     let max = 0;
 
@@ -40,53 +59,95 @@ consecutive = async () => {
         }
     }
     
-    console.log(max)
-
     return max
+}
+
+check_royal_flush = async () => {
+    let { hand, hand_values, hand_values_sorted } = await read_cards()
+    let royal = [8, 9, 10, 11, 12]
+    if (arrEq(royal, hand_values_sorted)) return true
+    return false
+}
+
+arrEq = (arr1, arr2) => {
+    let arrLength;
+    if ((arrLength = arr1.length) != arr2.length) return false;
+    for (let i = 0; i < arrLength; i++) if (arr1[i] !== arr2[i]) return false;
+    return true;
 }
 
 check_straight = async () => {
     let res = await consecutive()
-    if (res) return true
+    if (res === 5) return true
     return false
 }
 
 check_flush = async () => {
     
-    let { hands, hand_values, hand_values_sorted } = await read_cards()
-    let suits = hands.map(card => card.slice(-1))
+    let { hand, hand_values, hand_values_sorted } = await read_cards()
+    let suits = hand.map(card => card.slice(-1))
     let res = await suits.every(suit => suit === suits[0])
-    console.log(`check_flush: ${res}`)
+    return res
+}
+
+check_straight_flush = async () => {
+    let is_flush = await check_flush()
+    if (is_flush === true) {
+        let is_straight = await check_straight()
+        if (is_straight === true) return true
+    }
+
+    return false
+}
+
+hand_rank = async () => {
+
+    let freq = await frequencies()
+
+    let rank = 0
+    let pairs = 0
+    let hasTrips = false
+    let hasQuads = false
+    let hasFlush = false
+    let hasStraight = false
+    let isRoyal = false
+
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] === 2) pairs++
+        else if (freq[i] === 3) hasTrips = true
+        else if (freq[i] === 4) hasQuads = true
+    }
+
+    if (pairs < 1 && !hasTrips && !hasQuads) {
+        hasStraight = await check_straight()
+        hasFlush = await check_flush()
+        isRoyal = await check_royal_flush()
+    }
+
+    console.log(isRoyal)
+
+    if (hasFlush && isRoyal) rank = 10
+    else if (hasStraight && hasFlush) rank = 9
+    else if (hasQuads) rank = 8
+    else if (hasTrips && pairs == 1) rank = 7
+    else if (hasFlush) rank = 6
+    else if (hasStraight) rank = 5
+    else if (hasTrips) rank = 4
+    else if (pairs == 2) rank = 3
+    else if (pairs == 1) rank = 2
+    else rank = 1
+
+    return rank
 }
 
 ranks = async () => {
 
-    let { hands, hand_values, hand_values_sorted } = await read_cards()
+    let { hand, hand_values, hand_values_sorted } = await read_cards()
+    let rank = await hand_rank()
 
-    hand = {}
-    hand.pairs = 0
-    hand.trips = 0
-    hand.quads = 0
-    hand.hasFlush = false
-    hand.hasStraight = false
-
-    let count = {}
-    hand_values.forEach(i => count[i] = (count[i]||0) + 1);
-    console.log(count)
-
-    for (const prop in count) {
-        if (count[prop] === 2) hand.pairs++
-        else if (count[prop] === 3) hand.trips = 1
-        else if (count[prop] === 4) hand.quads = 1
-    }
-
-    console.log(hand)
+    console.log(`rank: ${rank}`)
 }
 
 
-
-
-
 ranks()
-check_straight().then(console.log)
-check_flush()
+check_straight_flush().then(res => console.log(`check_straight_flush: ${res}`))
