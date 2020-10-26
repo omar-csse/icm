b83bd72afee1589682762a1ffca983dbc43cdafb
omar <omar.csse@gmail.com>
2020-10-26 10:02:07 +1000

    finish the whole logic and bug fixes

--- poker/poker.js
+++ poker/poker.js
@@ -18,6 +18,19 @@ let PokerGame = class Poker {
         ['A', 12]
     ])
 
+    ranks = {
+        ROYALFLUSH: 10,
+        STRAIGHTFLUSH: 9,
+        QUADS: 8,
+        FULLHOUSE: 7,
+        FLUSH: 6,
+        STRAIGHT: 5,
+        TRIPS: 4,
+        TWOPAIRS: 3,
+        PAIR: 2,
+        HIGHCARD: 1
+    }
+
     readHands = async () => {
         
         return new Promise((resolve, reject) => {
@@ -36,14 +49,13 @@ let PokerGame = class Poker {
 
                 let hand_values = hands.map(card => this.cards.get(card[0]))
 
-                let player1_hand = hand_values.slice(0, 5).sort((a, b) => a = b)
-                let player2_hand = hand_values.slice(5, 10).sort((a, b) => a = b)
+                let player1_hand = hand_values.slice(0, 5).sort((a, b) => a - b)
+                let player2_hand = hand_values.slice(5, 10).sort((a, b) => a - b)
                 
                 this.player1_hands_sorted.push(player1_hand)
                 this.player2_hands_sorted.push(player2_hand)
     
             }).on('close', () => {
-                console.log(`Setup cards`)
                 resolve()
             }).on('error', (err) => {
                 reject(err)
@@ -68,15 +80,17 @@ let PokerGame = class Poker {
 
         let count = 1
         let max = 0;
+        let values = []
     
         for (let i = 0; i <= hand.length; i++) {
             if (hand[i] === hand[i-1] + 1) {
                 count++;
                 max < count ? max = count: count = 1;
+                values.push(hand[i])
             }
         }
         
-        return max
+        return { max: max, value: Math.max(...values) }
     }
 
     arrEq = (arr1, arr2) => {
@@ -86,23 +100,22 @@ let PokerGame = class Poker {
         return true;
     }
 
-    check_straight = async (hand) => {
-        let res = await this.consecutive(hand)
-        if (res === 5) return true
-        return false
+    check_straight = (hand) => {
+        let straight = this.consecutive(hand)
+        if (straight.max === 5) return { exist: true, card: straight.value }
+        return { exist: false, card: straight.value }
     }
 
-    check_flush = async (hand_original) => {
-    
+    check_flush = async (hand, hand_original) => {
         let suits = hand_original.map(card => card.slice(-1))
         let res = await suits.every(suit => suit === suits[0])
-        return res
+        return { exist: res, card: Math.max(...hand) }
     }
 
     check_royal_flush = async (hand) => {
         let royal = [8, 9, 10, 11, 12]
-        if (this.arrEq(royal, hand)) return true
-        return false
+        if (this.arrEq(royal, hand)) return {exist: true, card: 12}
+        return {exist: false, card: 12}
     }
 
     hand_rank = async (hand_original, hand) => {
@@ -110,43 +123,63 @@ let PokerGame = class Poker {
         let freq = this.frequencies(hand)
     
         let rank = 0
-        let pairs = 0
-        let hasTrips = false
-        let hasQuads = false
-        let hasFlush = false
-        let hasStraight = false
-        let isRoyal = false
+        let pairs_counter = 0
+        let pairs = {}
+        let trips = {}
+        let quads = {}
+        let flush = {}
+        let straight = {}
+        let royal = {}
     
         for (let i = 0; i < freq.length; i++) {
-            if (freq[i] === 2) pairs++
-            else if (freq[i] === 3) hasTrips = true
-            else if (freq[i] === 4) hasQuads = true
+            if (freq[i] === 2) pairs = {count: ++pairs_counter, card: i}
+            else if (freq[i] === 3) trips = {exist: true, card: i}
+            else if (freq[i] === 4) quads = {exist: true, card: i}
+        }
+
+        if (!trips.exist && !quads.exist) {
+            straight = this.check_straight(hand)
+            flush = await this.check_flush(hand, hand_original)
+            royal = await this.check_royal_flush(hand)
         }
     
-        if (pairs < 1 && !hasTrips && !hasQuads) {
-            hasStraight = await this.check_straight(hand)
-            hasFlush = await this.check_flush(hand_original)
-            isRoyal = await this.check_royal_flush(hand)
-        }
-    
-        if (hasFlush && isRoyal) rank = 10
-        else if (hasStraight && hasFlush) rank = 9
-        else if (hasQuads) rank = 8
-        else if (hasTrips && pairs == 1) rank = 7
-        else if (hasFlush) rank = 6
-        else if (hasStraight) rank = 5
-        else if (hasTrips) rank = 4
-        else if (pairs == 2) rank = 3
-        else if (pairs == 1) rank = 2
-        else rank = 1
+        if (flush.exist && royal.exist) rank = {rank: this.ranks.ROYALFLUSH, card: royal.card}
+        else if (straight.exist && flush.exist) rank = {rank: this.ranks.STRAIGHTFLUSH, card: straight.card}
+        else if (quads.exist) rank = {rank: this.ranks.QUADS, card: quads.card}
+        else if (trips.exist && pairs == 1) rank = {rank: this.ranks.FULLHOUSE, card: trips.card}
+        else if (flush.exist) rank = {rank: this.ranks.FLUSH, card: flush.card}
+        else if (straight.exist) rank = {rank: this.ranks.STRAIGHT, card: straight.card}
+        else if (trips.exist) rank = {rank: this.ranks.TRIPS, card: trips.card}
+        else if (pairs.count == 2) rank = {rank: this.ranks.TWOPAIRS, card: pairs.card}
+        else if (pairs.count == 1) rank = {rank: this.ranks.PAIR, card: pairs.card}
+        else rank = {rank: this.ranks.HIGHCARD, card: 0}
     
         return rank
+
     }
-    
-    same_rank = (hand1, hand2) => {
-        let hand1_max = Math.max.apply(Math, hand1);
-        let hand2_max = Math.max.apply(Math, hand2);
-        return hand1_max > hand2_max ? 1 : 2
+
+    check_rank = (p1, p2, hand1, hand2) => {
+
+        let hand1_new = hand1
+        let hand2_new = hand2
+
+        if (p1.rank !== 1 && p1.rank == p2.rank) {
+            if (p1.card == p2.card) {
+                hand1_new = hand1_new.filter(card => p1.card !== card)
+                hand2_new = hand2_new.filter(card => p1.card !== card)
+            } 
+            else {
+                return p1.card > p2.card ? 1 : 2
+            }
+        }
+        else if (p1.rank !== p2.rank) {
+            return p1.rank > p2.rank ? 1 : 2
+        }
+
+        for (let i = hand1_new.length - 1; i >= 0; i--) {
+            if (hand1_new[i] > hand2_new[i]) return 1
+            else if (hand2_new[i] > hand1_new[i]) return 2
+        }
     }
 
     play = async () => {
@@ -154,15 +187,11 @@ let PokerGame = class Poker {
         await this.readHands()
 
         for (let i = 0; i < this.player1_hands.length; i++) {
-            let rank_p1 = await this.hand_rank(this.player1_hands[i], this.player1_hands_sorted[i])
-            let rank_p2 = await this.hand_rank(this.player2_hands[i], this.player2_hands_sorted[i])
+            let p1 = await this.hand_rank(this.player1_hands[i], this.player1_hands_sorted[i])
+            let p2 = await this.hand_rank(this.player2_hands[i], this.player2_hands_sorted[i])
 
-            if (rank_p1 > rank_p2) this.player1_winning++
-            else if (rank_p2 > rank_p1) this.player2_winning++
-            else {
-                let winner = this.same_rank(this.player1_hands_sorted[i], this.player2_hands_sorted[i])
-                winner === 1 ? this.player1_winning++ : this.player2_winning++
-            }
+            let winner = this.check_rank(p1, p2, this.player1_hands_sorted[i], this.player2_hands_sorted[i])
+            winner === 1 ? this.player1_winning++ : this.player2_winning++
         }
 
         console.log(`player1: ${this.player1_winning} hands`)
